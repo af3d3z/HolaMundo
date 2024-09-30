d20e3efca9781bcb5b1ece8e27f90a61cb887732
th3-m0th <12004683-th3-m0th@users.noreply.gitlab.com>
2024-09-30 20:26:03 +0200

    Agregar archivos de proyecto.

---
+++ WinFormHolaMundo/Form1.vb
@@ -0,0 +1,7 @@
+﻿Imports BibliotecaDeClases
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Persona As New Persona
+    End Sub
+End Class
